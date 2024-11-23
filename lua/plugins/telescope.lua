af198c435edd13068c0a0a18890fb53e03269a2a
Rafał Klusek <klusek.raf@gmail.com>
2024-11-23 22:12:48 +0100

    lsp

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,6 @@
 return {
+  {
+
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
   dependencies = { { "nvim-lua/plenary.nvim"} },
@@ -7,5 +9,18 @@ return {
     vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
     vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
   end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
- 
