dac1afe0fb457dcfc9eaf716caeb2204edf86a52
Rafał Klusek <klusek.raf@gmail.com>
2024-11-23 23:39:05 +0100

    minor tweaks

--- lua/options.lua
+++ lua/options.lua
@@ -3,5 +3,14 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-
-
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.showmode = false
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 30
