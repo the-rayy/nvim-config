ccd239a01384d87b5350d6bf242c70b787f92b5d
Rafał Klusek <klusek.raf@gmail.com>
2024-11-24 01:32:51 +0100

    minor tweaks

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,16 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      close_if_last_window = false,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        }
+      }
+    })
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
   end
 }
